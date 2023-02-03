bc3688b4c302c79be097812704fb976bc44c47b5
Simon Morgan <simonmorgan@Admins-MacBook-Pro-2.local>
2023-02-03 16:55:44 +0000

    Convert 1 to numeral test passing

---
+++ src/test/java/com/techreturners/RomanNumeralsConverterTest.java
@@ -0,0 +1,15 @@
+package com.techreturners;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumeralsConverterTest {
+
+    @Test
+    public void convertOneToNumeral() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+
+        assertEquals("I", romanNumeralsConverter.convert(1));
+    }
+}
